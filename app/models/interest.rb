d43550064e2f64abb8067eb6124c1b66d15c005a
Alex Kim <supernovamaniac@gmail.com>
2015-10-17 12:02:09 -0400

    pulling and merging new animal model/index

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -3,13 +3,20 @@ class Interest < ActiveRecord::Base
 	belongs_to :animal
 	belongs_to :user
 
-	#validate :user_has_one_interest_per_animal, on: :create
-	
+	validate :user_has_one_interest_per_animal, on: :create
+	validate :user_cannot_show_interest_to_himself, on: :create
+
 	private
 	def user_has_one_interest_per_animal
 		return true if self.user.nil? || self.animal.nil?
 		unless Interest.where(animal_id: self.animal_id, user_id: self.user_id).to_a.empty?
 			errors.add(:base, "User already showed interest in this animal")
+		end
+	end
+
+	def user_cannot_show_interest_to_himself
+		if (self.animal.user_id == self.user_id)
+			errors.add(:base, "User cannot show interest to his/her own animal")
 		end
 	end
 end
